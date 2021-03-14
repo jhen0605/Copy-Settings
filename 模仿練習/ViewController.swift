d8a3049e9d2230e5cb7887be935dc83ccd690397
jhen0605 <amy95173@gmail.com>
2021-03-14 17:12:18 +0800

    Initial Commit

---
+++ 模仿練習/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  模仿練習
+//
+//  Created by 簡吟真 on 2021/3/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
